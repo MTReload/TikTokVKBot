ba03c3a5f5dd65ad723e0827e78e9544f63722f8
MTReload <suhonosik.dmitry@gmail.com>
2021-08-09 16:53:09 +0300

    Добавил логирование и активность "печатает"

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from vk_api.utils import get_random_id
 from vk_api import VkUpload
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType, VkBotMessageEvent
 
+import logging as log
+
 users = {}
 
 
@@ -25,11 +27,20 @@ def get_tt_video_id(url: str) -> str:
 
 
 def download_tt(tt_link) -> str:
+    l = log.getLogger("download_tt")
+    
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/39.0.2171.95 Safari/537.36'}
+    try:
+        response = requests.get(tt_link, headers=headers)
+    except Exception as e:
+        l.getChild('request').error(e)
+        raise e
     
-    response = requests.get(tt_link, headers=headers)
+    if response.status_code != 200:
+        l.getChild('response').error(f"status code is {response.status_code}")
+        raise Exception("can't get tt video id")
     
     api = TikTokAPI(cookie=response.cookies)
     cwd = os.getcwd()
@@ -55,21 +66,27 @@ def split_video(video_path: str, part_len=14) -> List[str]:
                   f" -ss 00:00:{str(part_len * i).zfill(2)} -t 00:00:{str(part_len).zfill(2)} " \
                   f" -c:v libx264 {fname} -y"
         print(command)
-        subprocess.run(command)
+        subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         ret.append(fname)
     os.remove(video_path)
     return ret
 
 
 def process_tt_msg(event: VkBotMessageEvent):
+    l = log.getLogger("process_tt_msg")
     peer_id = event.obj.get('peer_id')
     from_id = event.obj.get('from_id')
     tt_file = download_tt(text)
     vids = split_video(tt_file)
-    
+    vk.messages.setActivity(peer_id=peer_id, type="typing")
     # tt_video = uploader.story(vids[0], 'video', user_ids=[from_id])
-    tt_video = uploader.story(vids[0], 'video', group_id=GROUP_ID, link_text="more",
-                              link_url=r"https://vk.com/donut/public206314659")
+    try:
+        tt_video = uploader.story(vids[0], 'video', group_id=GROUP_ID, link_text="more",
+                                  link_url=r"https://vk.com/donut/public206314659")
+    except Exception as e:
+        l.error(e)
+        return
+    
     data = tt_video.json()
     attstr = f"story{data['response']['story']['owner_id']}_{data['response']['story']['id']}"
     
@@ -78,31 +95,38 @@ def process_tt_msg(event: VkBotMessageEvent):
                      random_id=get_random_id())
     
     for vid in vids[1:]:
-        uploader.story(vid, 'video', group_id=GROUP_ID)
+        try:
+            uploader.story(vid, 'video', group_id=GROUP_ID)
+        except Exception as e:
+            l.error(e)
     
     for v in vids:
-        os.remove(v)
+        try:
+            os.remove(v)
+        except Exception as e:
+            l.error(e)
 
 
 if __name__ == "__main__":
+    log.basicConfig(level=log.INFO)
+    l = log.getLogger("main")
     vk_session = vk_api.VkApi(token=os.getenv("BOT_TOKEN"))
     GROUP_ID = int(os.getenv("GROUP_ID"))
-    longpoll = VkBotLongPoll(vk_session, GROUP_ID, wait=-9)
+    longpoll = VkBotLongPoll(vk_session, GROUP_ID)
     vk = vk_session.get_api()
     uploader = VkUpload(vk)
     
     TIKTOK_SUBSTRING = 'https://vm.tiktok.com'
     
-    print("start")
+    l.info("start")
     
     while True:
         try:
-            print('listen')
+            l.info('listen')
             for event in longpoll.listen():
                 if event.type == VkBotEventType.MESSAGE_NEW:
                     text: str = event.obj.get('text')
                     sender = event.obj.get('from_id')
-                    print(event)
                     if text.find(TIKTOK_SUBSTRING) != -1:
                         if sender in users:
                             if (datetime.now() - users[sender]).seconds < 20:
@@ -110,4 +134,4 @@ if __name__ == "__main__":
                         users[sender] = datetime.now()
                         process_tt_msg(event)
         except Exception as e:
-            print(e)
+            l.error(e)
